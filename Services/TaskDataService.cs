4d85512c914dd9abaa5d9bd0c2628ac86e42e53c
Anjana <Anjana@vuedata.in>
2024-01-29 19:25:49 +0530

    Add Edit and Delete option

--- Services/TaskDataService.cs
+++ Services/TaskDataService.cs
@@ -17,30 +17,31 @@ namespace TMS.Service
         {
             return await _applicationDbContext.TaskDatas.ToListAsync();
         }
-        public async Task<bool> AddNewTaskData(TaskData taskData)
+        public async Task<TaskData> AddNewTaskData(TaskData taskData)
         {
-            taskData.Status = taskData.Status!=null? taskData.Status : "New";
+            taskData.Status = taskData.Status!=null || taskData.Status != "" ? taskData.Status : "To Do";
             taskData.DueDate = taskData.DueDate != DateTime.MinValue ? taskData.DueDate : DateTime.Now;
             taskData.CreatedBy = 1;
             taskData.CreatedOn = DateTime.Now;
             await _applicationDbContext.TaskDatas.AddAsync(taskData);
             await _applicationDbContext.SaveChangesAsync();
-            return true;
+            return taskData;
         }
         public async Task<TaskData> GetTaskDataById(int? id)
         {
             TaskData TaskData = await _applicationDbContext.TaskDatas.FirstOrDefaultAsync(x => x.Id == id);
             return TaskData;
         }
-        public async Task<bool> UpdateTaskDataDetail(TaskData taskData)
+        public async Task<TaskData> UpdateTaskDataDetail(TaskData taskData)
         {
             _applicationDbContext.TaskDatas.Update(taskData);
             await _applicationDbContext.SaveChangesAsync();
-            return true;
+            return taskData;
         }
-        public async Task<bool> DeleteTaskDataDetail(TaskData taskData)
+        public async Task<bool> DeleteTaskDataDetail(int id)
         {
-            _applicationDbContext.TaskDatas.Remove(taskData);
+            TaskData TaskData = await _applicationDbContext.TaskDatas.FirstOrDefaultAsync(x => x.Id == id);
+            _applicationDbContext.TaskDatas.Remove(TaskData);
             await _applicationDbContext.SaveChangesAsync();
             return true;
         }
